beed248ec0e74bd48db07bff912ab8788b2a490d
Keith Gautreaux <keith.gautreaux@gmail.com>
2022-07-12 14:14:38 -0700

    it can work I think

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,19 +1,18 @@
 export function Logo(props) {
   return (
-  <svg class="h-12 w-auto" viewBox="0, 0, 183, 48" {...props}>
+<svg class="h-12 w-auto" viewBox="0, 0, 183, 48" {...props}>
   <defs>
-    <linearGradient id="Gradient_1" gradientUnits="userSpaceOnUse" x1="56.135" y1="21.272" x2="120.141" y2="21.272" gradientTransform="matrix(0.797, 0.604, -0.604, 0.797, 0, 0)">
+    <linearGradient id="Gradient_1" gradientUnits="userSpaceOnUse" x1="19.549" y1="5.906" x2="43.255" y2="5.906" gradientTransform="matrix(0.818, 0.576, -0.576, 0.818, 0, 0)">
       <stop offset="0" stop-color="#76C6DA"/>
-      <stop offset="0.246" stop-color="#8FAEE6"/>
-      <stop offset="0.624" stop-color="#AF8FF5"/>
+      <stop offset="0.351" stop-color="#40BEF5"/>
       <stop offset="1" stop-color="#6021EB"/>
     </linearGradient>
   </defs>
   <g id="Layer_1">
-    <text transform="matrix(1, 0, 0, 1, 301.5, 83.5)">
-      <tspan x="-192.577" y="42" font-family="Avenir-Heavy" font-size="65" fill="#000000">QUILT</tspan>
+    <text transform="matrix(1, 0, 0, 1, 109.189, 25.5)">
+      <tspan x="-71.325" y="15" font-family="Avenir-Heavy" font-size="50" fill="#000000">QUILT</tspan>
     </text>
-    <path d="M77.142,91.772 L68.103,103.697 L58.962,96.769 L37.454,118.929 L24.95,109.453 L46.459,87.293 L39.945,82.356 L18.436,104.515 L6.038,95.119 L27.546,72.96 L19.666,66.987 L28.704,55.062 L38.055,62.149 L46.789,53.136 L39.014,47.243 L48.092,35.265 L57.39,42.312 L77.494,21.57 L89.893,30.966 L69.736,51.669 L76.25,56.606 L96.459,35.943 L108.91,45.379 L88.754,66.082 L96.319,71.816 L87.241,83.794 L78.205,76.945 L69.471,85.959 z M65.701,67.469 L59.187,62.532 L50.454,71.546 L57.021,76.522 z" fill="url(#Gradient_1)"/>
+    <path d="M29.868,30.636 L26.678,35.168 L23.204,32.723 L15.532,41.205 L10.78,37.86 L18.453,29.378 L15.977,27.636 L8.305,36.118 L3.593,32.801 L11.266,24.319 L8.271,22.211 L11.461,17.679 L15.015,20.181 L18.13,16.731 L15.176,14.651 L18.38,10.099 L21.913,12.587 L29.085,4.647 L33.796,7.964 L26.605,15.889 L29.08,17.632 L36.292,9.72 L41.023,13.051 L33.832,20.976 L36.707,23 L33.502,27.552 L30.069,25.135 L26.953,28.585 z M25.317,21.79 L22.842,20.047 L19.727,23.497 L22.222,25.254 z" fill="url(#Gradient_1)"/>
   </g>
 </svg>
   )
