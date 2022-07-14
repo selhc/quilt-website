440b01a701fea99c39a440fc652259a73b5a5f8f
Keith Gautreaux <keith.gautreaux@gmail.com>
2022-07-14 09:07:42 -0700

    remove placeholders

--- src/components/Actors.jsx
+++ src/components/Actors.jsx
@@ -1,22 +1,10 @@
 import Image from 'next/future/image'
 
 import { Container } from '@/components/Container'
-import logoLaravel from '@/images/logos/laravel.svg'
-import logoMirage from '@/images/logos/mirage.svg'
-import logoStatamic from '@/images/logos/statamic.svg'
-import logoStaticKit from '@/images/logos/statickit.svg'
-import logoTransistor from '@/images/logos/transistor.svg'
-import logoTuple from '@/images/logos/tuple.svg'
 import logoPrh from '@/images/logos/prh.png'
 import logoSjrmc from '@/images/logos/sjrmc.png'
 
 const actors = [
-  { name: 'Transistor', logo: logoTransistor },
-  { name: 'Tuple', logo: logoTuple },
-  { name: 'StaticKit', logo: logoStaticKit },
-  { name: 'Mirage', logo: logoMirage },
-  { name: 'Laravel', logo: logoLaravel },
-  { name: 'Statamic', logo: logoStatamic },
   { name: 'Pullman Regional Hospital', logo: logoPrh },
   { name: 'St. Joseph\'s Regional Medical Center', logo: logoSjrmc }
 ]
