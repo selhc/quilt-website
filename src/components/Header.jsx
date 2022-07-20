c80c6b998e503731955bba200b35e03facb7f097
Keith Gautreaux <keith.gautreaux@gmail.com>
2022-07-20 05:20:12 -0700

    meetings url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ const solutions = [
   {
     name: 'Meetings',
     description: 'Transcripts and recordings of QUILT meetings.',
-    href: 'https://meetings.quilt.healthcare',
+    href: 'https://quilt-healthcare.gitbook.io/meetings',
     icon: UserGroupIcon,
   },
   {
